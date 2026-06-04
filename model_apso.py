7b70649f3822483cedbed1230626360e3dfcb543
adeliayuan <lade04113@gmail.com>
2026-06-04 07:53:35 +0700

    Update model_apso.py

--- model_apso.py
+++ model_apso.py
@@ -1,32 +1,24 @@
-import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier
 
 
-# BEST PARAMETERS (HASIL APSO)
-
-def get_best_params():
-
-    return {
-        "n_estimators": 487,
-        "max_depth": 65,
-        "min_samples_split": 22,
-        "min_samples_leaf": 8,
-        "max_features": 0.42
-    }
-
-
-# BUILD MODEL
-
-def build_model():
-
-    params = get_best_params()
+def build_model(
+    n_estimators,
+    max_depth,
+    min_samples_split,
+    min_samples_leaf,
+    max_features
+):
+    """
+    Membangun model Extremely Randomized Trees
+    berdasarkan hyperparameter yang diberikan user.
+    """
 
     model = ExtraTreesClassifier(
-        n_estimators=params["n_estimators"],
-        max_depth=params["max_depth"],
-        min_samples_split=params["min_samples_split"],
-        min_samples_leaf=params["min_samples_leaf"],
-        max_features=params["max_features"],
+        n_estimators=int(n_estimators),
+        max_depth=int(max_depth),
+        min_samples_split=int(min_samples_split),
+        min_samples_leaf=int(min_samples_leaf),
+        max_features=float(max_features),
         random_state=42,
         n_jobs=-1
     )
